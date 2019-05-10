fc6264d6b8ec3d245146aeccfd895265c0d64aa7
victor bennich <vbennich@victors-mbp.clients.eduroam.liu.se>
2019-05-10 12:21:26 +0200

    added more tests

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/one', (req, res) => res.send("1"))
+
+app.get('/two', (req, res) => res.send("2"))
+
+app.get('/three', (req, res) => res.send("3"))
+
+app.get('/four', (req, res) => res.send("4"))
+
+app.get('/five', (req, res) => res.send("5"))
+
+app.get('/six', (req, res) => res.send("6"))
+
+app.get('/seven', (req, res) => res.send("7"))
+
+app.get('/eight', (req, res) => res.send("8"))
+
+app.get('/nine', (req, res) => res.send("9"))
+
+app.get('/ten', (req, res) => res.send("10"))
+
+app.listen(port, () => console.log(`Example" app listening on port ${port}!`))
 
 module.exports = app
--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 chai.should();
 describe("Tests", () => {
-    describe("GET /", () => {
+    describe("GET /1-10", () => {
         // Test to get all students record
         it("should return Hello World!", (done) => {
              chai.request(app)
@@ -19,5 +19,106 @@ describe("Tests", () => {
                      done();
                   });
          });
+         it("should return 1", (done) => {
+              chai.request(app)
+                  .get('/one')
+                  .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    expect(res.text).to.equal("1");
+                    done();
+                   });
+          });
+          it("should return 2", (done) => {
+               chai.request(app)
+                   .get('/two')
+                   .end((err, res) => {
+                     res.should.have.status(200);
+                     res.body.should.be.a('object');
+                     expect(res.text).to.equal("2");
+                     done();
+                    });
+           });
+           it("should return 3", (done) => {
+                chai.request(app)
+                    .get('/three')
+                    .end((err, res) => {
+                      res.should.have.status(200);
+                      res.body.should.be.a('object');
+                      expect(res.text).to.equal("3");
+                      done();
+                     });
+            });
+            it("should return 4", (done) => {
+                 chai.request(app)
+                     .get('/four')
+                     .end((err, res) => {
+                       res.should.have.status(200);
+                       res.body.should.be.a('object');
+                       expect(res.text).to.equal("4");
+                       done();
+                      });
+             });
+             it("should return 5", (done) => {
+                  chai.request(app)
+                      .get('/five')
+                      .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        expect(res.text).to.equal("5");
+                        done();
+                       });
+              });
+              it("should return 6", (done) => {
+                   chai.request(app)
+                       .get('/six')
+                       .end((err, res) => {
+                         res.should.have.status(200);
+                         res.body.should.be.a('object');
+                         expect(res.text).to.equal("6");
+                         done();
+                        });
+               });
+               it("should return 7", (done) => {
+                    chai.request(app)
+                        .get('/seven')
+                        .end((err, res) => {
+                          res.should.have.status(200);
+                          res.body.should.be.a('object');
+                          expect(res.text).to.equal("7");
+                          done();
+                         });
+                });
+                it("should return 8", (done) => {
+                     chai.request(app)
+                         .get('/eight')
+                         .end((err, res) => {
+                           res.should.have.status(200);
+                           res.body.should.be.a('object');
+                           expect(res.text).to.equal("8");
+                           done();
+                          });
+                 });
+                 it("should return 9", (done) => {
+                      chai.request(app)
+                          .get('/nine')
+                          .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            expect(res.text).to.equal("9");
+                            done();
+                           });
+                  });
+                  it("should return 10", (done) => {
+                       chai.request(app)
+                           .get('/ten')
+                           .end((err, res) => {
+                             res.should.have.status(200);
+                             res.body.should.be.a('object');
+                             expect(res.text).to.equal("10");
+                             done();
+                            });
+                   });
+
     });
 });
